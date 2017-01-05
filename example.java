8b1b194d33b9eb9bb5a34c5b64d3f077095188f0
shabbirtgh <shabbirtgh@gmail.com>
2017-01-05 17:47:10 +0530

    Add files via upload

---
+++ example.java
@@ -0,0 +1,12 @@
+class example {
+ int x; int y;
+ String name;
+ public static void main(String args[]) {
+ example pnt = new example();
+ System.out.println ("pnt is " + pnt.name + " " + pnt.x + " " + pnt.y);
+} }
+
+//Option d is correct
+
+// After creating object memory is created for the instance members and they can be called by the object.memberName
+//As the variable are not initialised so it will give the default values the the string i.e null and 0 for the integer
